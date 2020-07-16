6103fcebbdac5b27957e8afdc285c398cce6ef31
Fazila <fazila.kassim@hotmail.com>
2020-07-16 17:16:39 +0200

    ajout foncionnitées

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 const URL = "http://localhost:8000";
 var listHeroes = null;
-axios.get(URL + "/heroes")
+axios.get(URL + "/heroes?_sort=views&_order=asc")
     .then(heroes => displayAll(heroes.data))
     .catch(err => console.error(err));
 
@@ -8,20 +8,26 @@ axios.get(URL + "/heroes")
 
 
 function displayAll(heroes) {
-    console.log(heroes)
     const all = document.getElementById("all");
     listHeroes = heroes
-
-
+    all.innerHTML = "";
+    // console.log("JE SIS LA    ",
+    //     heroes.sort(function(a, b) {
+    //     return b.id - a.id;
+    // }))
     heroes.forEach((heroe, index) => {
-        if (index < 6) {
+        if (index < 21) {
             all.innerHTML += `
-        <li>${heroe.name}</li>
+        <div data-u-id="${heroe.id}" class="card">
+        <p>${heroe.name}</p>
+        <figure><img src="${heroe.image.url}"></figure>
         <button class="details" data-id="${heroe.id}">Details</button>
-        <button class="supprimer" data-id="${heroe.id}> Supprimer</button>
-        
+        <button class="supprimer" data-id="${heroe.id}">Delete</button>
+        <button class="edit" data-id="${heroe.id}">Edit</button>
+        </div>
         `;
         }
+        // 
 
     });
 
@@ -36,18 +42,35 @@ function displayAll(heroes) {
     buttonsDelete.forEach(button => {
         button.onclick = suppimerHeroe
     })
-console.log(buttonsDelete)
+
+
+    
+    const buttonsEdit = document.querySelectorAll(".edit");
+
+    buttonsEdit.forEach(button => {
+        button.onclick = editrHeroe
+    })
+
+
 
 }
-
 
 
 function suppimerHeroe (evt){
-    const id = evt.target.getAttribute("data-id")
-    axios.delete(URL + "/" + id) 
-    .then()
+    const id = evt.target.getAttribute("data-id");
+    console.log(URL + "/heroes/" + id)
+    axios.delete(URL + "/heroes/" + id) 
+    .then(res => {
+        displayAll(listHeroes)
+        removeUserFromDocument(id);
+    })
     .catch(error => console.error(error))
+
+    
+    
 }
+
+
 
 function openModale(evt) {
 
@@ -74,3 +97,59 @@ function openModale(evt) {
 
 
 }
+
+
+function editrHeroe(evt){
+    const id = evt.target.getAttribute("data-id");
+}
+
+const createButton = document.getElementById("create").onclick = modaleCreateHeroe;
+function modaleCreateHeroe(){
+    
+    const modale = document.createElement("div");
+    modale.classList.add("modaleCreate");
+    const body = document.querySelector("body");
+    body.appendChild(modale)
+    modale.innerHTML = `
+    <input id="name" type="text" name="name" placeholder="name">
+    <input  id="fullname" type="text" name="full-name" placeholder="Full Name">
+    <input  id="gender" type="text" name="gender" placeholder="Gender">
+    <input  id="combat" type="text" name="combat" placeholder="Combat">
+    <input  id="alignment" type="text" name="alignment" placeholder="Alignement">
+    <input  id="race" type="text" name="race" placeholder="Race">
+    <input  id="publisher" type="text" name="publisher" placeholder="Publisher">
+    <button id="valider" name="valider">créer</button>
+    `;
+
+    const valider = document.getElementById("valider");
+    valider.onclick = createHeroe;
+
+}
+function createHeroe(evt){
+   const name = document.getElementById("name").value
+   const fullName = document.getElementById("fullname").value
+   const gender = document.getElementById("gender").value
+   const combat = document.getElementById("combat").value
+   const alignment = document.getElementById("alignment").value
+   const race = document.getElementById("race").value
+   const publisher = document.getElementById("publisher").value
+   console.log(name, " ", fullName, " ", gender, " ", combat, " ", alignment, " ", race, " ", publisher)
+    
+   axios.post(URL + "/heroes/", { 
+        name,
+        fullName,
+        gender,
+        combat,
+        alignment,
+        race,
+        publisher
+    })
+    .then()
+    .catch(err => console.error(err))
+   
+}
+function removeUserFromDocument(idUser) {
+    // https://developer.mozilla.org/fr/docs/Web/CSS/S%C3%A9lecteurs_d_attribut
+    const cardToRemove = document.querySelector(`[data-u-id="${idUser}"]`);
+    cardToRemove.remove();
+  }
